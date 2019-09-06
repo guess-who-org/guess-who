356f222acbd6ec81f761f8702dc9df8efae64266
ChristianMacDonald <chrismacd412@gmail.com>
2019-09-05 23:03:40 -0400

    Implemented post request to /api/auth/login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const model = require('../database/guessWhoModel');
+const secrets = require('../config/secrets');
 
 const router = express.Router();
 
@@ -10,15 +11,25 @@ router.post('/register', validateUser, async (req, res) => {
         let credentials = req.body;
         let hash = bcrypt.hashSync(credentials.password, 14);
         credentials.password = hash;
-        let user = await model.insert(credentials);
+        let user = await model.insertUser(credentials);
         res.status(201).json(user);
     } catch (err) {
         res.status(500).json({ error: "There was a problem saving the user to the database." });
     }
 });
 
-router.post('/login', validateUser, (req, res) => {
-    res.send("Not implemented yet, but valid request body was sent.");
+router.post('/login', validateUser, async (req, res) => {
+    try {
+        let user = await model.getUsersByName(req.body.username);
+        if (user && bcrypt.compareSync(req.body.password, user.password)) {
+            let token = generateToken(user);
+            res.status(200).json(token);
+        } else {
+            res.status(401).json({ message: 'Invalid credentials.' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: "There was a problem logging in." });
+    }
 });
 
 function validateUser(req, res, next) {
@@ -35,6 +46,17 @@ function validateUser(req, res, next) {
     } else {
         res.status(400).json({ message: 'Missing user data.' });
     }
+}
+
+function generateToken(user) {
+    let payload = {
+        subject: user.id,
+        username: user.username
+    };
+    let options = {
+        expiresIn: '1h'
+    };
+    return jwt.sign(payload, secrets.jwtSecret, options);
 }
 
 module.exports = router;
