92f2fa015e088963e1a04af3dc850ebffe86ae46
the-best-developer <im.dylan.gosnell@gmail.com>
2019-09-05 22:45:10 -0400

    Added celeb CRUD model

--- database/guessWhoModel.js
+++ database/guessWhoModel.js
@@ -4,8 +4,28 @@ const getUsers = _ => {
     return db('users');
 }
 
-const getUsersByName = (name) => {
+const getUserByName = (name) => {
     return db("users").where({ name: name });
+}
+
+const getCelebs = _ => {
+    return db('celebs');
+}
+
+const getCelebById = (id) => {
+    return db("celebs").where({ id: id });
+}
+
+const addCeleb = (name) => {
+    return db('celebs').insert({ name: name })
+}
+
+const updateCeleb = (id, name) => {
+    return db('celebs').where({ id: id }).update({name: name });
+}
+
+const deleteCeleb = (id) => {
+    return db('celebs').where({ id: id }).del();
 }
 
 module.exports = {
