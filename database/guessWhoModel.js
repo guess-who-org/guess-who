4b99f30fa1e84916c79d013678bb95a2e3bb66aa
the-best-developer <im.dylan.gosnell@gmail.com>
2019-09-05 23:21:22 -0400

    Added CRUD functions for the rest of the tables

--- database/guessWhoModel.js
+++ database/guessWhoModel.js
@@ -1,5 +1,7 @@
 const db = require('./guessWhoConfig.js');
 
+// Users table
+
 const getUsers = _ => {
     return db('users');
 }
@@ -12,6 +14,8 @@ const insertUser = async user => {
     let [id] = await db("users").insert(user);
     return db("users").where({ id }).first();
 }
+
+// Celebs table
 
 const getCelebs = _ => {
     return db('celebs');
@@ -33,9 +37,111 @@ const deleteCeleb = (id) => {
     return db('celebs').where({ id: id }).del();
 }
 
+// Tweets table
+
+const getTweets = _ => {
+     return db('tweets');
+}
+    
+const getTweetById = (id) => {
+    return db("tweets").where({ id: id });
+}
+
+const addTweet = (contents) => {
+    return db('tweets').insert(contents)
+}
+
+const updateTweet = (id, contents) => {
+    return db('tweets').where({ id: id }).update(contents);
+}
+
+const deleteTweet = (id) => {
+    return db('tweets').where({ id: id }).del();
+}
+
+// Games table
+
+const getGames = _ => {
+    return db('games');
+}
+
+const getGameById = (id) => {
+    return db("games").where({ id: id });
+}
+
+const addGame = (contents) => {
+    return db('games').insert(contents)
+}
+
+const updateGame = (id, contents) => {
+    return db('games').where({ id: id }).update(contents);
+}
+
+const deleteGame = (id) => {
+    return db('games').where({ id: id }).del();
+}
+
+// Games_celebs table
+
+const getGamesCelebs = _ => {
+    return db('games_celebs');
+}
+
+const getGamesCelebsById = (id) => {
+    return db("games_celebs").where({ id: id });
+}
+
+const addGameCeleb = (contents) => {
+    return db('games_celebs').insert(contents)
+}
+
+const deleteGameCeleb = (id) => {
+    return db('games_celebs').where({ id: id }).del();
+}
+
+// Games_celebs table
+
+const getGamesUsers = _ => {
+    return db('games_celebs');
+}
+
+const getGamesUsersById = (id) => {
+    return db("games_celebs").where({ id: id });
+}
+
+const addGameUser = (contents) => {
+    return db('games_celebs').insert(contents)
+}
+
+const deleteGameUser = (id) => {
+    return db('games_celebs').where({ id: id }).del();
+}
+
 module.exports = {
     getUsers,
     getUsersByName,
     insertUser,
     getCelebs,
+    getCelebById,
+    addCeleb,
+    updateCeleb,
+    deleteCeleb,
+    getTweets,
+    getTweetById,
+    addTweet,
+    updateTweet,
+    deleteTweet,
+    getGames,
+    getGameById,
+    addGame,
+    updateGame,
+    deleteGame,
+    getGamesCelebs,
+    getGamesCelebsById,
+    addGameCeleb,
+    deleteGameCeleb,
+    getGamesUsers,
+    getGamesUsersById,
+    addGameUser,
+    deleteGameUser
 }
