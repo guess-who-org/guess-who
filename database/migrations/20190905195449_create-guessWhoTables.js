edfd03b4fcadcfa4ba562dd55e77fb6acefd8bcd
the-best-developer <im.dylan.gosnell@gmail.com>
2019-09-05 22:11:14 -0400

    Added tweets, celebs, games, and intermediary tables

--- database/migrations/20190905195449_create-guessWhoTables.js
+++ database/migrations/20190905195449_create-guessWhoTables.js
@@ -1,13 +1,71 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('users', users => {
-        users.increments();
-        users.string('username', 256).notNullable().unique();
-        users.string('password', 256).notNullable().unique();;
-    });
+    return knex.schema.createTable('users', table => {
+        table.increments();
+        table.string('username', 256).notNullable().unique();
+        table.string('password', 256).notNullable().unique();;
+    })
+    .createTable('celebs', table => {
+        table.increments();
+        table.string('name', 256).notNullable().unique();
+    })
+    .createTable('tweets', table => {
+        table.increments();
+        table.string('tweet', 256).notNullable();
+        table.integer('celeb_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('celebs')
+    })
+    .createTable('games', table => {
+        table.increments();
+        table.string('title', 256).notNullable();
+        table.integer('points').defaultTo('0');
+        table.integer('winner')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+    })
+    .createTable('games_celebs', table => {
+        table.integer('game_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('games')
+        table.integer('celeb_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('celebs')
+            .notNullable();
+        table.primary(['game_id', 'celeb_id']);
+    })
+    .createTable('games_users', table => {
+        table.integer('game_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('games')
+        table.integer('user_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('userss')
+            .notNullable();
+        table.primary(['game_id', 'user_id']);
+    })
 };
   
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('users');
+    return (
+        knex.schema
+        .dropTableIfExists('users')
+        .dropTableIfExists('celebs')
+        .dropTableIfExists('tweets')
+        .dropTableIfExists('games')
+        .dropTableIfExists('games_celebs')
+        .dropTableIfExists('games_users')
+    );
 };
   
