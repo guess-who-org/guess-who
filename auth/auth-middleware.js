1e042151c908c0c20bf18c35969c735751e954f9
ChristianMacDonald <chrismacd412@gmail.com>
2019-09-09 21:26:30 -0400

    Created auth-middleware.js

---
+++ auth/auth-middleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
+const authMiddleware = (req, res, next) => {
+    if (req.headers.token) {
+        try {
+            jwt.verify(req.headers.token, secrets.jwtSecret);
+            next();
+        } catch (err) {
+            res.status(401).json({ message: 'Invalid token.' });
+        }
+    } else {
+        res.status(400).json({ message: 'Missing token.' });
+    }
+}
+
+module.exports = authMiddleware;
