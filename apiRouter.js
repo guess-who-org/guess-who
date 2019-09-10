9cbf78ff7c179038d98e3e4eab66f9f4edadd76d
the-best-developer <im.dylan.gosnell@gmail.com>
2019-09-09 21:30:44 -0400

    Added users endpoint at /api/users

--- apiRouter.js
+++ apiRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authRouter = require('./auth/auth-router');
+const userRouter = require('./users/userRouter');
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
+router.use('/users', userRouter);
 
 module.exports = router;
