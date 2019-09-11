3f654a648971589fbd157b91ea15f364e79af177
ChristianMacDonald <chrismacd412@gmail.com>
2019-09-10 21:18:46 -0400

    Made port dynamic

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config()
+;
 const server = require('./server');
 
-server.listen(8000, () => {
-    console.log('Server listening on port 8000.');
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
 });
